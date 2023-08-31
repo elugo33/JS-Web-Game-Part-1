a697cef34823c2964fd240d59f97a08be315a1f3
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-08-31 14:20:00 -0500

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position='fixed'
+pineTree.style.left='450px'
+pineTree.style.bottom='200px'
+document.body.append(pineTree)
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+let shield = newImage('img')
+shield.src = 'assets/shield.png'
+shield.style.bottom = '185px'
+shield.style.left ='165px'
+
+let staff=newImage('img')
+staff.src='assets/staff.png'
+staff.style.left= '600px'
+staff.style.bottom= '100px'

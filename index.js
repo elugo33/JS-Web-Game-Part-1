978d771fab9a3147672d2e78c4241e8d4b347199
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-09-02 12:51:20 -0500

    final commit

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left,bottom){
+let object = document.createElement('img')
+object.src = url
+object.style.position='fixed'
+object.style.left=left + 'px'
+object.style.bottom = bottom + 'px'
+document.body.append(object)
+return object
+} 
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position='fixed'
-pineTree.style.left='450px'
-pineTree.style.bottom='200px'
-document.body.append(pineTree)
+function newItem(url, left, bottom){
+let object = newImage(url, left, bottom)
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
-
-sword.addEventListener('click', function(){
-    sword.remove()
+object.addEventListener('dblclick', () => {
+    object.remove()
 })
 
-let shield = newImage('img')
-shield.src = 'assets/shield.png'
-shield.style.bottom = '185px'
-shield.style.left ='165px'
 
-let staff=newImage('img')
-staff.src='assets/staff.png'
-staff.style.left= '600px'
-staff.style.bottom= '100px'
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png',350, 100)
+newImage('assets/pine-tree', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 600, 100)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+}

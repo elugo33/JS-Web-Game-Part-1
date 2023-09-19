ee5eab1b83dbfaa4b60799e66e410d8b667be9ec
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-09-19 13:00:50 -0500

    final final

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ object.addEventListener('dblclick', () => {
     object.remove()
 })
 
-
+}
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png',350, 100)
-newImage('assets/pine-tree', 450, 200)
+newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 600, 100)
 
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
-}
